a4dcfef148d5b075239e0fde8dc176ddc29394fc
hbuckner <bladeastro@gmail.com>
2016-11-29 17:03:51 -0500

    testing branch system

--- mydirectory/group7_hw7/onevoter.cc
+++ mydirectory/group7_hw7/onevoter.cc
@@ -14,6 +14,8 @@ static const string kTag = "ONEVOTER: ";
 **/
 OneVoter::OneVoter() {}
 /****************************************************************
+ * this is a test
+ */
 **/
 OneVoter::OneVoter(int sequence, int arrival_seconds, int duration_seconds) {
   sequence_ = sequence;
