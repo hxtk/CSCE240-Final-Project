4765e4c05205a0f557a6a78b0479fefc42f78cd1
hxtk <pgsanders@protonmail.com>
2016-11-30 20:56:48 -0500

    Minor style changes
    Added vertical space between blocks, indented comments

--- mydirectory/group7_hw7/onevoter.h
+++ mydirectory/group7_hw7/onevoter.h
@@ -20,10 +20,11 @@ class OneVoter {
   /****************************************************************
    * Constructors and destructors for the class.
   **/
-// OneVoter emulates the basics functions of a voter at the voting booth. 
-// It takes in sequence, arrival_seconds, and duration_seconds    
-// and initializes sequence_, time_arrival_seconds,time_start_voting_seconds_,
-// time_vote_duration_seconds_ and which_station.
+
+  // OneVoter emulates the basics functions of a voter at the voting booth. 
+  // It takes in sequence, arrival_seconds, and duration_seconds    
+  // and initializes sequence_, time_arrival_seconds,time_start_voting_seconds_,
+  // time_vote_duration_seconds_ and which_station.
   OneVoter();
   OneVoter(int sequence, int arrival_seconds, int duration_seconds);
   virtual ~OneVoter();
@@ -40,14 +41,16 @@ class OneVoter {
    * General functions.
   **/
 
-// AssignStation is responsible for taking in station_number and    
-// start_time_seconds and calculating which_station, 	 
-// time_vote_duration_seconds, and time_waiting_seconds_.
+  // AssignStation is responsible for taking in station_number and    
+  // start_time_seconds and calculating which_station, 	 
+  // time_vote_duration_seconds, and time_waiting_seconds_.
   void AssignStation(int station_number, int start_time_seconds);
-// dead code no DoneVoting function in .cc file
+  
+  // dead code no DoneVoting function in .cc file
   void DoneVoting();
-// GetTimeInQ uses time_start_voting_seconds_ and time_arrival_seconds_ to
-// calculate the wait time of a voter in the line.
+  
+  // GetTimeInQ uses time_start_voting_seconds_ and time_arrival_seconds_ to
+  // calculate the wait time of a voter in the line.
   int GetTimeInQ() const;
 
   string ToString();
@@ -65,9 +68,10 @@ class OneVoter {
   /****************************************************************
    * General private functions.
   **/
-//ConvertTime(takes in time_in_seconds and returns the time of day in the hour:minute:second format
+  // ConvertTime(takes in time_in_seconds and returns the time of day in the hour:minute:second format
   string ConvertTime(int time_in_seconds) const;
-//GetTOD takes time_in_seconds and returns the time of day.
+  
+  // GetTOD takes time_in_seconds and returns the time of day.
   string GetTOD(int time) const;
 };
 
